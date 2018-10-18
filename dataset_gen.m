070718c31f0b5c1fff9dd7b15c2e4e27c2648744
Tian Yu <tianyu@qxslab.cn>
2018-10-18 17:15:05 +0800

    Generate First DataSet
    Signed-off-by: Tian Yu <tianyu@qxslab.cn>

--- dataset_gen.m
+++ dataset_gen.m
@@ -1,5 +1,5 @@
 clear
-batch = 50e3;
+batch = 100e3;
 symbols = 128;
 sps = 8;
 sym_rate = 1e6;
@@ -31,25 +31,25 @@ sprintf('CPFSK2')
 [~, CPFSK2] = generate_signal('CPFSK', 2, batch, symbols, sps, sym_rate, snr);
 CPFSK2 = single(CPFSK2);
 
-sprintf('CPFSK4')
-[~, CPFSK4] = generate_signal('CPFSK', 4, batch, symbols, sps, sym_rate, snr);
-CPFSK4 = single(CPFSK4);
+% sprintf('CPFSK4')
+% [~, CPFSK4] = generate_signal('CPFSK', 4, batch, symbols, sps, sym_rate, snr);
+% CPFSK4 = single(CPFSK4);
+% 
+% sprintf('CPFSK8')
+% [~, CPFSK8] = generate_signal('CPFSK', 8, batch, symbols, sps, sym_rate, snr);
+% CPFSK8 = single(CPFSK8);
 
-sprintf('CPFSK8')
-[~, CPFSK8] = generate_signal('CPFSK', 8, batch, symbols, sps, sym_rate, snr);
-CPFSK8 = single(CPFSK8);
-
-sprintf('CPM2')
-[~, CPM2] = generate_signal('CPM', 2, batch, symbols, sps, sym_rate, snr);
-CPM2 = single(CPM2);
-
-sprintf('CPM4')
-[~, CPM4] = generate_signal('CPM', 4, batch, symbols, sps, sym_rate, snr);
-CPM4 = single(CPM4);
-
-sprintf('CPM8')
-[~, CPM8] = generate_signal('CPM', 8, batch, symbols, sps, sym_rate, snr);
-CPM8 = single(CPM8);
+% sprintf('CPM2')
+% [~, CPM2] = generate_signal('CPM', 2, batch, symbols, sps, sym_rate, snr);
+% CPM2 = single(CPM2);
+% 
+% sprintf('CPM4')
+% [~, CPM4] = generate_signal('CPM', 4, batch, symbols, sps, sym_rate, snr);
+% CPM4 = single(CPM4);
+% 
+% sprintf('CPM8')
+% [~, CPM8] = generate_signal('CPM', 8, batch, symbols, sps, sym_rate, snr);
+% CPM8 = single(CPM8);
 
 sprintf('GFSK')
 [~, GFSK] = generate_signal('GFSK', 0, batch, symbols, sps, sym_rate, snr);
@@ -63,49 +63,47 @@ sprintf('MSK')
 [~, MSK] = generate_signal('MSK', 0, batch, symbols, sps, sym_rate, snr);
 MSK = single(MSK);
 
-sprintf('FSK2')
-[~, FSK2] = generate_signal('M-FSK', 2, batch, symbols, sps, sym_rate, snr);
-FSK2 = single(FSK2);
-
-sprintf('FSK4')
-[~, FSK4] = generate_signal('M-FSK', 4, batch, symbols, sps, sym_rate, snr);
-FSK4 = single(FSK4);
-
-sprintf('FSK8')
-[~, FSK8] = generate_signal('M-FSK', 8, batch, symbols, sps, sym_rate, snr);
-FSK8 = single(FSK8);
+% sprintf('FSK2')
+% [~, FSK2] = generate_signal('M-FSK', 2, batch, symbols, sps, sym_rate, snr);
+% FSK2 = single(FSK2);
+% 
+% sprintf('FSK4')
+% [~, FSK4] = generate_signal('M-FSK', 4, batch, symbols, sps, sym_rate, snr);
+% FSK4 = single(FSK4);
+% 
+% sprintf('FSK8')
+% [~, FSK8] = generate_signal('M-FSK', 8, batch, symbols, sps, sym_rate, snr);
+% FSK8 = single(FSK8);
 
 sprintf('BPSK')
 [~, BPSK] = generate_signal('BPSK', 0, batch, symbols, sps, sym_rate, snr);
 BPSK = single(BPSK);
 
-sprintf('DBPSK')
-[~, DBPSK] = generate_signal('DBPSK', 0, batch, symbols, sps, sym_rate, snr);
-DBPSK = single(DBPSK);
+% sprintf('DBPSK')
+% [~, DBPSK] = generate_signal('DBPSK', 0, batch, symbols, sps, sym_rate, snr);
+% DBPSK = single(DBPSK);
+% 
 
-sprintf('DQPSK')
-[~, DQPSK] = generate_signal('DQPSK', 0, batch, symbols, sps, sym_rate, snr);
-DQPSK = single(DQPSK);
 
-sprintf('DPSK2')
-[~, DPSK2] = generate_signal('M-DPSK', 2, batch, symbols, sps, sym_rate, snr);
-DPSK2 = single(DPSK2);
+% sprintf('DPSK2')
+% [~, DPSK2] = generate_signal('M-DPSK', 2, batch, symbols, sps, sym_rate, snr);
+% DPSK2 = single(DPSK2);
+% 
+% sprintf('DPSK4')
+% [~, DPSK4] = generate_signal('M-DPSK', 4, batch, symbols, sps, sym_rate, snr);
+% DPSK4 = single(DPSK4);
+% 
+% sprintf('DPSK8')
+% [~, DPSK8] = generate_signal('M-DPSK', 8, batch, symbols, sps, sym_rate, snr);
+% DPSK8 = single(DPSK8);
 
-sprintf('DPSK4')
-[~, DPSK4] = generate_signal('M-DPSK', 4, batch, symbols, sps, sym_rate, snr);
-DPSK4 = single(DPSK4);
+% sprintf('PSK2')
+% [~, PSK2] = generate_signal('M-PSK', 2, batch, symbols, sps, sym_rate, snr);
+% PSK2 = single(PSK2);
 
-sprintf('DPSK8')
-[~, DPSK8] = generate_signal('M-DPSK', 8, batch, symbols, sps, sym_rate, snr);
-DPSK8 = single(DPSK8);
-
-sprintf('PSK2')
-[~, PSK2] = generate_signal('M-PSK', 2, batch, symbols, sps, sym_rate, snr);
-PSK2 = single(PSK2);
-
-sprintf('PSK4')
-[~, PSK4] = generate_signal('M-PSK', 4, batch, symbols, sps, sym_rate, snr);
-PSK4 = single(PSK4);
+% sprintf('PSK4')
+% [~, PSK4] = generate_signal('M-PSK', 4, batch, symbols, sps, sym_rate, snr);
+% PSK4 = single(PSK4);
 
 sprintf('PSK8')
 [~, PSK8] = generate_signal('M-PSK', 8, batch, symbols, sps, sym_rate, snr);
@@ -119,17 +117,12 @@ sprintf('OFDM2')
 [~, OFDM2] = generate_signal('OFDM', 2, batch, symbols, sps, sym_rate, snr);
 OFDM2 = single(OFDM2);
 
-sprintf('OFDM4')
-[~, OFDM4] = generate_signal('OFDM', 4, batch, symbols, sps, sym_rate, snr);
-OFDM4 = single(OFDM4);
+% sprintf('OFDM4')
+% [~, OFDM4] = generate_signal('OFDM', 4, batch, symbols, sps, sym_rate, snr);
+% OFDM4 = single(OFDM4);
+% 
+% sprintf('OFDM8')
+% [~, OFDM8] = generate_signal('OFDM', 8, batch, symbols, sps, sym_rate, snr);
+% OFDM8 = single(OFDM8);
 
-sprintf('OFDM8')
-[~, OFDM8] = generate_signal('OFDM', 8, batch, symbols, sps, sym_rate, snr);
-OFDM8 = single(OFDM8);
-
-batch = 1;
-symbols = 128;
-sps = 8;
-sym_rate = 1e6;
-snr = 50;
 save(sprintf('batch%d_symbols%d_sps%d_baud%d_snr%d.dat', batch, symbols, sps, sym_rate/1e6,snr))
